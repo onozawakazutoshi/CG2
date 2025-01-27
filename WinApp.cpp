33444027d64de4a0744e3113bbb490d13fda31d0
onozawakazutoshi <146906422+onozawakazutoshi@users.noreply.github.com>
2025-01-27 15:29:38 +0900

    //完成

--- WinApp.cpp
+++ WinApp.cpp
@@ -1,9 +1,64 @@
 #include "WinApp.h"
+#include "externals/imgui/imgui_impl_win32.cpp"
 
 void WinApp::Initialize()
 {
+	const int32_t kClientWidth = 1280;
+	const int32_t kClientHeight = 720;
+	RECT wrc;
+	WNDCLASS wc{};
+	HWND hwnd_;
+
+	
+
+	wc.lpfnWndProc = WindowProc;
+	//ウィンドウクラス名（何でもよい）
+	wc.lpszClassName = L"CG2WindowClass";
+	//インスタンスハンドル
+	wc.hInstance = GetModuleHandle(nullptr);
+	//カーソル
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+	//ウィンドウクラスを登録する
+	RegisterClass(&wc);
+
+
+
+	wrc = { 0,0,kClientWidth,kClientHeight };
+
+	hwnd_ = CreateWindow(
+		wc.lpszClassName,
+		L"CG2",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		wrc.right - wrc.left,
+		wrc.bottom - wrc.top,
+		nullptr,
+		nullptr,
+		wc.hInstance,
+		nullptr
+	);
 }
 
 void WinApp::Update()
 {
+}
+
+LRESULT CALLBACK WinApp::WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam)) {
+		return true;
+	}
+	//メッセージに応じてゲーム固有の処理を行う
+	switch (msg) {
+		//ウィンドウが破棄された
+	case WM_DESTROY:
+		//OSに対して、アプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	//標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
--- WinApp.h
+++ WinApp.h
@@ -1,8 +1,35 @@
 #pragma once
+#include <Windows.h>
+#include <wrl.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#define DIRECTINPUT_VERSION    0x0800
+#include <dinput.h>
+#include "externals/imgui/imgui.h"
+#include "externals/imgui/imgui_impl_dx12.h"
+#include "externals/imgui/imgui_impl_win32.h"
+
+
+#include <Windows.h>
+#include <winuser.h>
+#include <cstdint>
+#include <string>
+#include <format>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
+#include <cmath>
+
+#pragma  comment(lib,"dinput8.lib")
+#pragma  comment(lib,"dxguid.lib")
+
 class WinApp
 {
 public:
+	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 	void Initialize();
 	void Update();
+
+	
 };
 
